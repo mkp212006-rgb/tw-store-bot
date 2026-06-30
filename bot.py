bbe5636959a15a3da09d8473858d27cd8c43f02d
mkp212006-rgb <mkp212006@gmail.com>
2026-06-29 23:49:53 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1304,13 +1304,13 @@ def quantidade_manual_para_int(texto: str) -> int | None:
 def margem_percentual_por_quantidade(quantidade: int) -> int:
     quantidade = int(quantidade)
     if 100 <= quantidade <= 400:
-        return 70
+        return 60
     if 401 <= quantidade <= 1000:
-        return 45
-    if 1001 <= quantidade <= 2000:
-        return 50
-    if 2001 <= quantidade <= 5000:
         return 35
+    if 1001 <= quantidade <= 2000:
+        return 45
+    if 2001 <= quantidade <= 5000:
+        return 25
     if 5001 <= quantidade <= 10000:
         return 25
     raise ValueError("Quantidade fora da faixa permitida")
